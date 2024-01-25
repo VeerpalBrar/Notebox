bff843a9ee2e2c4e077cb3c81871199fea355943
Veerpal Brar <veerpal707@gmail.com>
2024-01-25 08:38:11 -0500

    use venv in crawl script

--- notebox/crawler/crawl.sh
+++ notebox/crawler/crawl.sh
@@ -3,6 +3,7 @@
 current_date_time=$(date)
 echo "$current_date_time : Starting crawler"
 cd /Users/veerpalbrar/notebox
+source ./notebox/elastic/venv/bin/activate
 python3 -m notebox.crawler.crawler
 # ensure launchd doesn't think the job crashed due to fast run
 sleep 10 
