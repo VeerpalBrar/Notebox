0edc4ad4e1a35f619f04b0f77acf23258f33b32f
Veerpal Brar <veerpal707@gmail.com>
2024-02-05 18:46:26 -0500

    truncate result of server

--- notebox/server/search.py
+++ notebox/server/search.py
@@ -1,4 +1,4 @@
-from re import M
+import re
 from flask import Blueprint, request
 from ..elastic.elasticClient import ElasticClient
 
@@ -8,7 +8,18 @@ elastic = ElasticClient()
 @bp.route("/search")
 def get_post():
     """Search for files with certain content"""
-    return elastic.search(request.args.get('txt'))
+    txt = request.args.get('txt')
+    results = elastic.search(txt)
+    regex = r"(.{0,25})" + re.escape(txt) + r"(.{0,25})"
+
+    ret = []
+    for result in results:
+        found = re.findall(regex, str(result))
+        for f in  found:
+            ret.append("{} {} {}".format(f[0], txt, f[1]))
+    
+    return ret
+    
 
 
 
